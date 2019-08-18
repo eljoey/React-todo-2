8b316d936a32560a9c62b86c101d0ab7b926783c
Joe Hayden <eljoeyy@gmail.com>
2019-08-18 17:04:53 -0500

    Updates

---
+++ precache-manifest.f31f598fecf7cbf2b98f66e43ca8d8b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe55b2db0d1c8292c45cc6f7cd524f55",
+    "url": "/React-todo-2/index.html"
+  },
+  {
+    "revision": "8524547b0bba55a0cc68",
+    "url": "/React-todo-2/static/css/main.73ef8051.chunk.css"
+  },
+  {
+    "revision": "8060b0a0527b2b0a6f63",
+    "url": "/React-todo-2/static/js/2.308da5e9.chunk.js"
+  },
+  {
+    "revision": "8524547b0bba55a0cc68",
+    "url": "/React-todo-2/static/js/main.353ac75c.chunk.js"
+  },
+  {
+    "revision": "895d419a80daf05071b5",
+    "url": "/React-todo-2/static/js/runtime~main.4dd66a44.js"
+  }
+]);
